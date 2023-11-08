8da0e8020197497286d1c12b6343f12a6102cb04
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-11-08 15:06:58 +0100

    Update script.js

--- const/script.js
+++ const/script.js
@@ -41,3 +41,16 @@
 // cars = ["Toyota", "Volvo", "Audi"];    // ERROR
 
 // TODO Constant Objects
+// You can create a const object:
+// const car = {type:"Fiat", model:"500", color:"white"};
+
+// You can change a property:
+// car.color = "red";
+
+// You can add a property:
+// car.owner = "Johnson";
+//----------------------------------------------------------------
+// const car = {type:"Fiat", model:"500", color:"white"};
+
+// car = {type:"Volvo", model:"EX60", color:"red"}; //! ERROR
+
