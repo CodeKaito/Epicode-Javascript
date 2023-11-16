fb29f23e2f5f3f391e04b3e8d2d1b57de94babc5
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-11-16 09:37:11 +0100

    modified:   script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function multiply(number) {
-    let abs = Math.abs(number); // numero in val assoluto
-    let power = String(abs); // numero in string
-    let powerLength = power.length; // numero delle caselle della stringa
-    let Powerfive = Math.pow(5, powerLength); // potenza di 5 con il numero delle caselle dell input
-    console.log(Powerfive);
-    console.log(number);
-    console.log(number*Powerfive); //moltiplica
-  }
 
-multiply(10);
