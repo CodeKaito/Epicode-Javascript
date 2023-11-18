901890dea5d7f7fa3b09b71fcd5535dc41e9c65b
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-11-18 10:17:01 +0100

    modified:   codewars/EpicodeEsercizi/script.js

--- codewars/EpicodeEsercizi/script.js
+++ codewars/EpicodeEsercizi/script.js
@@ -22,12 +22,73 @@
 // Input: array = [3, 7, -2, 5, 8, 1, 2, 5, 6, -4]
 // Output: [8, 7, 6, 5, 5, 3, 2, 1, -4, -2]
 
-// function sortArray(array) {
-//     array.sort().reverse();
-//     console.log(array);
+// function sortArray(items, metodo) {
+//     let newArray = []; // Dichiaro un array vuoto per contenere gli elementi ordinati
+//     let minValue = items[0]; // Inizializzo il valore minimo con il primo elemento dell'array
+//     let maxValue = -Infinity; // Inizializzo il valore massimo come infinito
+//     let valueIndex = 0; // Inizializzo l'indice dell'elemento con valore minimo
+
+//     if (metodo === "crescente") {
+//         while (items.length > 0) { // Effettuo un ciclo finché ci sono ancora elementi nell'array originale
+//             minValue = items[0]; // Reinizializzo il valore minimo con il primo elemento dell'array
+//             valueIndex = 0; // Reinizializzo l'indice dell'elemento con valore minimo
+
+//             for (let i = 0; i < items.length; i++) { // Itero all'interno degli elementi di items
+//                 if (items[i] < minValue) { // Confronto il valore minimo con il primo elemento dell'array
+//                     minValue = items[i]; // Assegno il valore trovato alla variabile minValue
+//                     valueIndex = i; // Assegno ad una variabile valueIndex l'indice dell'elemento trovato cosi da rimuoverlo in un secondo momento
+//                 }
+//             }
+//             newArray.push(minValue); // Aggiungo il valore minore all'array ordinato uno alla volta
+//             items.splice(valueIndex, 1); // Rimuovo l'elemento con valore minimo dall'array originale e riparte l'interazione del while
+//         }
+//     } else if (metodo === "decrescente"){
+//         while (items.length > 0) { // Effettuo un ciclo finché ci sono ancora elementi nell'array originale
+//             maxValue = items[0]; // Reinizializzo il valore minimo con il primo elemento dell'array
+//             valueIndex = 0; // Reinizializzo l'indice dell'elemento con valore minimo
+
+//             for (let i = 0; i < items.length; i++) { // Itero all'interno degli elementi di items
+//                 if (items[i] > maxValue) { // Confronto il valore minimo con il primo elemento dell'array
+//                     maxValue = items[i]; // Assegno il valore trovato alla variabile minValue
+//                     valueIndex = i; // Assegno ad una variabile valueIndex l'indice dell'elemento trovato cosi da rimuoverlo in un secondo momento
+//                 }
+//             }
+//             newArray.push(maxValue); // Aggiungo il valore minore all'array ordinato uno alla volta
+//             items.splice(valueIndex, 1); // Rimuovo l'elemento con valore minimo dall'array originale e riparte l'interazione del while
+//         }
+//     }
+//     // Consollogghiamo l'array ordinato
+//     console.log(newArray);
+//     return newArray;
 // }
 
-// sortArray([3, 7, -2, 5, 8, 1, 2, 5, 6, -4])
+// sortArray([3, 7, -2, 5, 8, 1, 2, 5, 6, -1], "decrescente");
+
+// function trovaValoreMinimo(array) {
+//     // Verifica se l'array è vuoto
+//     if (array.length === 0) {
+//       return undefined; // Ritorna undefined se l'array è vuoto
+//     }
+  
+//     // Inizializza il valore minimo con il primo elemento dell'array
+//     let minimo = array[0];
+  
+//     // Itera attraverso gli elementi dell'array
+//     for (let i = 1; i < array.length; i++) {
+//       // Se l'elemento corrente è minore del valore minimo, aggiorna il valore minimo
+//       if (array[i] < minimo) {
+//         minimo = array[i];
+//       }
+//     }
+  
+//     // Ritorna il valore minimo trovato
+//     return minimo;
+//   }
+  
+//   // Esempio di utilizzo
+//   let numeri = [5, 2, 9, 1, 7];
+//   let minimo = trovaValoreMinimo(numeri);
+//   console.log("Il valore minimo è:", minimo); // Output: Il valore minimo è: 1
 
 // Variante:
 // Prova ad ordinali in modo crescente.
@@ -96,37 +157,37 @@
 //   in caso di perdita dovrà essere mostrato il messaggio "Mi dispiace, hai perso!".
 //   (generazione numeri random https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
 
-function tombolaMagica(randomArray, chosenArray) {
-    let resultArray = [];
-    for (let i = 0; i < chosenArray.length; i++) {
-        if (randomArray.includes(chosenArray[i]) === true) {
-            resultArray.push(chosenArray[i]);
-        }
-    }
+// function tombolaMagica(randomArray, chosenArray) {
+//     let resultArray = [];
+//     for (let i = 0; i < chosenArray.length; i++) {
+//         if (randomArray.includes(chosenArray[i]) === true) {
+//             resultArray.push(chosenArray[i]);
+//         }
+//     }
 
-    if (resultArray.length === 0 || resultArray.length === 1) {
-        console.log("Ritenta, sarai piú fortunato!");
-    } else if (resultArray.length === 2) {
-        console.log(resultArray + " AMBO!");
-    } else if (resultArray.length === 3) {
-        console.log(resultArray + " TERNA!");
-    } else if (resultArray.length === 4) {
-        console.log(resultArray + " QUATERNA");
-    } else if (resultArray.length === 5) {
-        console.log(resultArray + "CINQUINA!");
-    } else if (resultArray.length === chosenArray.length) {
-        console.log(resultArray + "TOMBOLA!");
-    }
-}
+//     if (resultArray.length === 0 || resultArray.length === 1) {
+//         console.log("Ritenta, sarai piú fortunato!");
+//     } else if (resultArray.length === 2) {
+//         console.log(resultArray + " AMBO!");
+//     } else if (resultArray.length === 3) {
+//         console.log(resultArray + " TERNA!");
+//     } else if (resultArray.length === 4) {
+//         console.log(resultArray + " QUATERNA");
+//     } else if (resultArray.length === 5) {
+//         console.log(resultArray + "CINQUINA!");
+//     } else if (resultArray.length === chosenArray.length) {
+//         console.log(resultArray + "TOMBOLA!");
+//     }
+// }
 
-function rand() {
-    let randomArray = [];
-     for (let i = 0; i < 10; i++) {
-        randomArray.push(Math.floor(Math.random()*90));
-     }
-     return randomArray;
-}
+// function rand() {
+//     let randomArray = [];
+//      for (let i = 0; i < 10; i++) {
+//         randomArray.push(Math.floor(Math.random()*90));
+//      }
+//      return randomArray;
+// }
 
-let computer = rand();
-let user = [1, 10, 25, 12, 32, 45, 90, 39, 67, 42]
-tombolaMagica(computer, user);
+// let computer = rand();
+// let user = [1, 10, 25, 12, 32, 45, 90, 39, 67, 42]
+// tombolaMagica(computer, user);
